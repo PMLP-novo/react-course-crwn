3bf998e131aa968d5e68bc63b9ba0249b0cacb8e
PMLP (Peter Mølgaard Pallesen) <pmlp@novonordisk.com>
2021-01-05 09:19:56 +0100

    lesson 70

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './homepage.scss'
+import Directory from '../../components/directory'
+
+function HomePage(){
+    return (
+        <div className='homepage'>
+            <Directory/>
+        </div>
+    )
+
+}
+
+export {HomePage}
