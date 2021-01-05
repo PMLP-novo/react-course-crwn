cb63c08aece0a038c1f6dbfef20a3e8e64563631
PMLP (Peter Mølgaard Pallesen) <pmlp@novonordisk.com>
2021-01-05 10:01:44 +0100

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import {HomePage} from './pages/homepage/homepage'
-
+import {Route} from 'react-router-dom';
+function HatsPage(){
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
     </div>
   );
 }
