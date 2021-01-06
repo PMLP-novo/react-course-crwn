4e71bc4c34e31beb5c0407aac607af2419b4fa56
PMLP (Peter Mølgaard Pallesen) <pmlp@novonordisk.com>
2021-01-06 12:01:45 +0100

    sign in

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './SignIn.scss'
 import FormInput from '../FormInput'
 import CustomButton from '../CustomButton'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 //import { Test } from './SignIn.styles';
 
 
@@ -14,13 +14,21 @@ class SignIn extends React.Component{
     password:''
     }
   }
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault()
-    this.setState({
-      email:'',
-      password:''
-      })
-  }
+    const {email, password} = this.state
+    try{
+      await auth.signInWithEmailAndPassword(email,password)
+      this.setState({
+        email:'',
+        password:''
+        })
+      }catch(error){
+      alert(error.message)
+    }
+
+    }
+    
   handleChange = event =>{
     const {value, name} = event.target;
     this.setState({[name]:value})
