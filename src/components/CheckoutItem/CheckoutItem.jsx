640bc20add173fee4c191f78b4895306b8a196df
PMLP (Peter Mølgaard Pallesen) <pmlp@novonordisk.com>
2021-01-07 16:38:39 +0100

    update-cart chechout

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './CheckoutItem.scss'
+import {connect} from 'react-redux'
+import {clearCartItem,removeCartItem,addCartItem} from '../../redux/cart.store'
 
-const CheckoutItem = ({item}) => (
+const CheckoutItem = ({dispatch,item}) => (
   <div className="checkout-item">
     <div className='image-container'>
       <img src={item.imageUrl} alt='item'/>
     </div>
     <span className='name'>{item.name}</span>
-    <span className='quantity'>{item.quantity}</span>
+    <span className='quantity'>
+      <div className='arrow' onClick={()=>dispatch(removeCartItem(item))}>&#10094;</div>
+    {item.quantity}
+    <div className='arrow' onClick={()=>dispatch(addCartItem(item))}>&#10095;</div>
+    </span>
     <span className='price'>${item.price}</span>
-    <div className='remove'>&#10005;</div>
+    <div className='remove' onClick={()=>dispatch(clearCartItem(item.id))}>&#10005;</div>
   </div>
 );
 
 
-export default CheckoutItem;
+export default connect()(CheckoutItem);
+//</div>onClick={()=>dispatch(addCartItem(item))}
--- src/redux/cart.store.jsx
+++ src/redux/cart.store.jsx
@@ -14,6 +14,16 @@ function addItemToCart(currentItems, newItem){
     }
 }
 
+function removeItemFromCart(currentItems, ItemToChange){
+    if (ItemToChange.quantity>1){
+        return currentItems.map(
+            (item)=>({ ...item, quantity:item.quantity - (item.id === ItemToChange.id)})
+            )
+    }else{
+        return currentItems.filter(item=>item.id!==ItemToChange.id)
+    }
+}
+
 export function cartReducer(state=INITIAL_STATE, action){
     switch (action.type){
         case 'TOGGLE_CART_HIDDEN':
@@ -21,6 +31,10 @@ export function cartReducer(state=INITIAL_STATE, action){
         case 'ADD_CART_ITEM':
             return {...state,
                 items:addItemToCart(state.items,action.payload)}
+        case "CLEAR_CART_ITEM":
+            return {...state,items:state.items.filter(item=>item.id!==action.payload)}
+        case "REMOVE_CART_ITEM":
+                return {...state,items:removeItemFromCart(state.items,action.payload)}
         default:
             return state
     }
@@ -33,6 +47,14 @@ export function toggleCartHidden(){
 
 export function addCartItem(item){
     return {type:"ADD_CART_ITEM",payload:item}
+}
+
+export function clearCartItem(id){
+    return {type:"CLEAR_CART_ITEM", payload:id}
+}
+
+export function removeCartItem(item){
+    return {type:"REMOVE_CART_ITEM", payload:item}
 }
 
 const selectCart = state=>state.cart;
