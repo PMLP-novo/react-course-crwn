5e114e508137db57b354c9f4b0b1e8f521eb1d82
PMLP (Peter Mølgaard Pallesen) <pmlp@novonordisk.com>
2021-01-07 16:56:39 +0100

    add persistens

--- src/redux/root-reducer.jsx
+++ src/redux/root-reducer.jsx
@@ -1,4 +1,15 @@
 import {combineReducers} from 'redux'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import {userReducer} from "./user.store"
 import {cartReducer} from "./cart.store"
-export default combineReducers({'user':userReducer, "cart":cartReducer})
+
+const persistsConfig ={
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
+
+const rootReducer = combineReducers({'user':userReducer, "cart":cartReducer})
+
+export default persistReducer(persistsConfig, rootReducer)
